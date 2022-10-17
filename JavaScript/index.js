8ec70c7c3dc7d34728c8b623fdcf6a0bddb018f1
Vitor Silva <developer.programation@oultook.com>
2022-10-17 17:33:03 -0300

    Calculadora

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,47 +1,4 @@
 
-/*const form = document.getElementById('orderForm');
-
-form.addEventListener("submit", function(event) {
-
-    event.preventDefault();
-
-    
-
-    const resposta = document.getElementById('resultado')
-    
-
-    const name = document.querySelector("input[name='name']").value
-    const address = document.querySelector("input[name='address']").value
-    const breadType = document.querySelector("select[name='breadType']").value
-    const main = document.querySelector("input[name='main']:checked").value
-    const obs = document.querySelector("textarea[name='observations']").value
-
-
-    var salad = ""
-    document.querySelectorAll("input[name='salad']:checked").forEach(function (item) {
-            salad += " - " + item.value + "\n"
-    });
-
-    console.log ({
-
-        name,
-        address,
-        breadType,
-        main,
-        obs
-    })
-
-   resposta.innerText = 
-        "\nPedido Realizado!" +
-        "\nNome do cliente: " + name +
-        "\nEndereço de entrega: " + address +
-        "\nTipo de pão: " + breadType +
-        "\nRecheio: \n - " + main + "\n" + salad +
-        "Observações: " + obs  
-
-    
-}) */
-
 //DESAFIO CADASTROS DEVs
 
 function createLabel(text, htmlFor){
